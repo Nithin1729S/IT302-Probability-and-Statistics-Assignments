90910147ad2da5f9d6fa39f73d975a9ce8cafe83
Nithin1729S <sureshnithin1729@gmail.com>
2024-11-06 16:59:27 +0530

    endsem lab

--- Lab_2/4_d.R
+++ Lab_2/4_d.R
@@ -6,3 +6,12 @@ probabilities <- dbinom(successes, n, p)
 barplot(probabilities, names.arg=successes, 
         main="Histogram of Binomial Distribution",
         xlab="Number of Successful Operations", ylab="Probability")
+
+#A new surgical procedure is said to be successful 80% of the time. Suppose
+#the operation is performed five times and the results are to be independent
+#of one another. Write a R/Python code to find the probability that
+#(a) All five operations are successful.
+#(b) Exactly four are successful.
+#(c) Less than two are successful.
+#(d) Plot the histogram of the corresponding distribution
+
